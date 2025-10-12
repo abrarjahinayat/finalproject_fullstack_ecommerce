50fe6be90b3c2c57033a91f7c4b598dc2b859f6a
Abrar Jahin Ayat <abrarjahinayat123@gmail.com>
2025-10-12 21:39:23 +0600

    multerImgUpload new function created for reusable components

--- src/route/api/banner/index.js
+++ src/route/api/banner/index.js
@@ -1,42 +1,14 @@
 const express = require("express");
-const addbannerControllers = require("../../../controllers/bannerControllers");
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads");
-  },
-
-  filename: function (req, file, cb) {
-    const randomtext = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    let fileextension = file.originalname.split(".").pop();
-    cb(null, file.fieldname + "-" + randomtext + "." + fileextension);
-  },
-});
-
 const path = require("path");
 
-function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png|gif/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb("Error: Images only! (jpeg, jpg, png, gif)");
-  }
-}
-
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1024 * 1024 * 5 },
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
-});
+const addbannerControllers = require("../../../controllers/bannerControllers");
+const multerImgUpload = require("../../../utils/multer.img.upload");
 
 const router = express.Router();
+
+
+const upload = multerImgUpload();
+
 
 router.post("/addbanner", upload.single("banner"), addbannerControllers);
 
